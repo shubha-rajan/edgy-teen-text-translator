a8b7875d29f8ff0947c2faa98a447329d8016a2e
Shubha Rajan <shubharajan94@gmail.com>
2019-03-21 11:15:44 -0700

    began to implement 'misspellchek' to semi-randomly make common spelling errors

--- edgify.js
+++ edgify.js
@@ -12,17 +12,36 @@ $(document).ready(function() {
          
         var sentencePattern = /\b[A-Z][A-Za-z0-9\s]+(\.|\!|\?)/g;
         var properNounPattern = /\b[A-Z][a-z]+\b/g;
-        var thePattern = /\bthe\b/gi
-        var thisPattern = /\bthis\b/gi
+        var thePattern = /\bthe\b/gi;
+        var thisPattern = /\bthis\b/gi;
+        var mePattern = /\bme\b/gi;
+        var silentEPattern= /e\b/gi;
+           
+        
         var slightlyEdgier = notEdgy.replace(sentencePattern, function(x){
             return "~*~*" + x + "*~*~";
         });
+
+        
         slightlyEdgier = slightlyEdgier.replace(properNounPattern, function(x){
             return "Xx" + x + "xX";
         });
         slightlyEdgier = slightlyEdgier.replace(/!/g, "!!1!!!1!");
         slightlyEdgier = slightlyEdgier.replace(thePattern, "teh");
         slightlyEdgier = slightlyEdgier.replace(thisPattern, "dis");
+        slightlyEdgier = slightlyEdgier.replace(mePattern, "meh");
+
+        slightlyEdgier = slightlyEdgier.replace(silentEPattern, function(x){
+            switch(Math.floor(Math.random()*2)) {
+                case 0:
+                    return "";
+                    break;
+                case 1:
+                    return "ee";
+                    break;
+            }
+        });
+        
         var edgyArray = slightlyEdgier.split(" ");
         edgyArray.forEach(function(word, index, array) {
             var letters = word.split("");
